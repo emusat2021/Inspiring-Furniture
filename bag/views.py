4d70520c01383c6f3eb0f845ef699ff49c043d29
Eugenia Musat <musat.eugenia@gmail.com>
2022-02-10 15:55:02 +0100

    Add adjust and remove in bag urls and bag views

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 def view_bag(request):
     """A view that renders the bag contents page"""
@@ -10,7 +10,6 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-
     bag = request.session.get('bag', {})
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
@@ -19,3 +18,29 @@ def add_to_bag(request, item_id):
     
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        bag = request.session.get('bag', {})
+        bag.pop(item_id)
+        request.session['bag'] = bag
+        return HttpResponse(status=202)
+    except Exception as e:
+        return HttpResponse(status=500)
