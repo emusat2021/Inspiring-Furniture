6164de89b7e0a9d2f7d51d470bdfb1194e50e29a
Eugenia Musat <musat.eugenia@gmail.com>
2022-02-16 12:55:14 +0100

    Add info app

---
+++ info/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def info(request):
+    """ Information table of contents """
+    template = 'info/info.html'
+    context = {}
+
+    return render(request, template, context)
+
+
+def how_to_buy(request):
+    """ Information regarding how to buy products """
+    template = 'info/how_to_buy.html'
+    context = {}
+
+    return render(request, template, context)
--- inspiring_furniture/settings.py
+++ inspiring_furniture/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'info',
 
      # Other
     'crispy_forms',
